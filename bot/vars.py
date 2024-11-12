969511d48cccec08b184b08d26ae35d35de3d10b
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-11-12 20:54:19 +0530

    Update vars.py

--- bot/vars.py
+++ bot/vars.py
@@ -1,7 +1,7 @@
 import os
 
 
-ADMINS = set(int(x) for x in os.environ.get("ADMINS", "").split())
-DATABASE_URL = os.environ.get("DATABASE_URL")
+ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1740287480").split())
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://susantamusic:susantabhan@cluster0.5pwi1py.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Translator-Bot")
 DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")
